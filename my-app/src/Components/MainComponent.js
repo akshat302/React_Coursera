9474b1ec3d8a625fafe9778b5b6340a1f7f58a42
akshat302 <akshatguptamac@gmail.com>
2021-05-21 23:49:20 +0530

    Assignment-2

--- my-app/src/Components/MainComponent.js
+++ my-app/src/Components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 
 
@@ -66,6 +67,7 @@ class Main extends Component {
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={ () => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" /> 
         </Switch>
  
